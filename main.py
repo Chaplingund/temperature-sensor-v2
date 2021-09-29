75018adefb4374900e2dcd68ebdda6bef8a68457
Chaplingund <77300464+Chaplingund@users.noreply.github.com>
2021-09-29 12:34:44 +0200

    aaa

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 voltage_raw = 0
-voltage = 0
-resistance = 0
+u = 0
+r = 0
+i = 0
 
 def on_forever():
-    global voltage_raw, voltage, resistance
+    global voltage_raw, u, r, i
     # imports amount of voltage, from 0-1023
     voltage_raw = pins.analog_read_pin(AnalogPin.P0)
     basic.show_number(voltage_raw)
     # maps amount of voltage to a 0-3 scale
-    voltage = Math.map(voltage_raw, 0, 1023, 0, 3)
-    # triggers on minimum amount of voltage reached
-    resistance = (3 - voltage) / 0.16
+    u = Math.map(voltage_raw, 0, 1023, 0, 3)
+    # calculates current
+    i = u/1000
+    # calculates resitance
+    r = (3 - u) / i
+    
+    basic.show_number(r)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
 let voltage_raw = 0
-let voltage = 0
-let resistance = 0
+let u = 0
+let r = 0
+let i = 0
 basic.forever(function on_forever() {
     
     //  imports amount of voltage, from 0-1023
     voltage_raw = pins.analogReadPin(AnalogPin.P0)
     basic.showNumber(voltage_raw)
     //  maps amount of voltage to a 0-3 scale
-    voltage = Math.map(voltage_raw, 0, 1023, 0, 3)
-    //  triggers on minimum amount of voltage reached
-    resistance = (3 - voltage) / 0.16
+    u = Math.map(voltage_raw, 0, 1023, 0, 3)
+    //  calculates current
+    i = u / 1000
+    //  calculates resitance
+    r = (3 - u) / i
+    basic.showNumber(r)
 })
