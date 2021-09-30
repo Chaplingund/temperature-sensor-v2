13468b49b0ec0131c609b9004f8717d014ae5a73
Chaplingund <77300464+Chaplingund@users.noreply.github.com>
2021-09-30 08:43:45 +0200

    renamed U

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 r = 0
 i = 0
 u = 0
-voltage_raw = 0
+u_raw = 0
 r_target = 100
 
 def on_forever():
-    global voltage_raw, u, i, r
+    global u_raw, u, i, r
     u_raw = 0
     # imports amount of voltage, from 0-1023
-    voltage_raw = pins.analog_read_pin(AnalogPin.P0)
+    u_raw = pins.analog_read_pin(AnalogPin.P0)
     basic.show_number(u_raw)
     # maps amount of voltage to a 0-3 scale
-    u = Math.map(voltage_raw, 0, 1023, 0, 3)
+    u = Math.map(u_raw, 0, 1023, 0, 3)
     # calculates current
     i = u / 1000
     # calculates resistance
@@ -19,19 +19,19 @@ def on_forever():
     # triggers on too high resistance
     if r >= r_target:
         basic.show_leds("""
-            . # . . .
-                        . # . # .
-                        . # . # .
-                        . # . # .
-                        . # . # .
+            . . # . .
+                        . # # # .
+                        # # # # #
+                        . . # . .
+                        . . # . .
         """)
         pins.digital_write_pin(DigitalPin.P1, 1023)
-    else: 
+    else:
         basic.show_leds("""
-                    . # . . .
-                                . # . # .
-                                . # . # .
-                                . # . # .
-                                . # . # .
-                """)
+            . # . # .
+                        . # . # .
+                        . . . . .
+                        # . . . #
+                        . # # # .
+        """)
 basic.forever(on_forever)
